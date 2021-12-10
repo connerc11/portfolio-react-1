d4d7daca990256e061cf804667b2d952e5585f35
Conner Cochrane <cccochrane1@knights.ucf.edu>
2021-12-10 14:04:09 -0500

    updated portfolio

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -13,13 +13,22 @@ function About() {
     
     <p id="two" className="lead">My name is Conner and you can explore some of my projects here! I am currently 21 years of age and I am in my final semester at the University of Central Florida. I am a very big fan of fishing, football and hockey, and enjoy editing and designing videos in my spare time. I am receiving a bachelors degree from UCF in Health Sciences Pre-Clinical. My favorite class that I have ever taken has been Organic Chemistry 2, and I also have a love for Anatomy. I am currently taking an online bootcamp in Software Development from the University of Central Florida. Feel free to reach out using the contact page! </p>
 
-    <h4>Github Links!</h4>
-    <a href="https://github.com/connerc11/Trivia-Brews-Project1">Trivia Brews</a><br></br>
-    <a href="https://github.com/connerc11/Fitness-Buddy">Fitness Buddy</a><br></br>
-    <a href="https://github.com/connerc11/Conner-Cochrane-Portfolio">Original Portfolio</a><br></br>
-    <a href="https://github.com/connerc11/Note-Taker">Note Taker</a><br></br>
-    <a href="https://github.com/connerc11/My-React-Portfolio">React Portfolio</a><br></br>
-    <a href="https://github.com/connerc11/distilled-news">Distilled News Hub</a>
+    <h4>Project and Github Links Links!</h4>
+    <a href="https://github.com/connerc11/Trivia-Brews-Project1">Trivia Brews Github</a><br></br>
+    <a href="https://connerc11.github.io/Trivia-Brews-Project1/">Trivia Brews Project</a><br></br>
+    <h5>Project Inclusions: HTML, CSS, JavaScript </h5>
+    <a href="https://github.com/connerc11/Fitness-Buddy">Fitness Buddy Github</a><br></br>
+    <a href="https://fitness-buddy-2.herokuapp.com/">Fiitness Buddy Project</a><br></br>
+    <h5>Project Inclusions: HTML, CSS, Node, MySQl, Handlebars </h5>
+    <a href="https://github.com/connerc11/Conner-Cochrane-Portfolio">Original Portfolio Github</a><br></br>
+    <a href="https://connerc11.github.io/Conner-Cochrane-Portfolio/">Original Portfolio Poject</a><br></br>
+    <h5>Project Inclusions: HTML, CSS </h5>
+    <a href="https://github.com/connerc11/Note-Taker">Note Taker Github</a><br></br>
+    <a href="https://note-taker1001.herokuapp.com/">Note Taker App</a><br></br>
+    <h5>Project Inclusions: HTML, CSS, JavaScript  </h5>
+    <a href="https://github.com/connerc11/distilled-news">Distilled News Hub Github</a>
+    <a href="https://sleepy-headland-32009.herokuapp.com/">Distilled News Hub Project</a><br></br>
+    <h5>Project Inclusions: HTML, CSS, React Jsx, MongoDb, Bootstrap</h5>
   </div>
 </div>
       //   <div className="bio my-5">
--- src/components/Resume/index.js
+++ src/components/Resume/index.js
@@ -9,9 +9,9 @@ import resume from "../../assets/files/general-resume.pdf"
     <h1 className="display-4 text-center">My Resume</h1>
     <h3 className="display-8">Coding Talents</h3>
     <hr className="my-4 text-center"></hr>
-    <p className="lead">Front and Back end Web Development</p>
-    <p className="lead">HTML, CSS, Javascript</p>
-    <p className="lead">NoSQL, SQL, MongoDB</p>
+    <p className="lead">Full Stack Web Development</p>
+    <p className="lead">HTML, CSS, Javascript, Handlebars</p>
+    <p className="lead">NoSQL, MySQL, MongoDB</p>
     <p className="lead">React with JSX</p>
     <a href={resume}> Download PDF </a>
   </div>
